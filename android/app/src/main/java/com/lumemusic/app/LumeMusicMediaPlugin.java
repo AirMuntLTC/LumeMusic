d6540477c979fd395151b6c703eaf723988ca778
AirMuntLTC <airmountcompany@gmail.com>
2026-09-11 20:47:03 +0800

    Fix Capacitor PluginMethod import

--- android/app/src/main/java/com/lumemusic/app/LumeMusicMediaPlugin.java
+++ android/app/src/main/java/com/lumemusic/app/LumeMusicMediaPlugin.java
@@ -14,7 +14,7 @@ import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import com.getcapacitor.annotation.PluginMethod;
+import com.getcapacitor.PluginMethod;
 import com.google.common.util.concurrent.ListenableFuture;
 
 @UnstableApi
